d4177e1597c0b2aa281cae06a4d30709ff7550b2
Andrey Chikilev <andrej.chikilev@gmail.com>
2017-07-05 11:52:55 +0300

    detail news view added

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import NewsViewList
+from .views import NewsViewList, NewsDetailView
 
 urlpatterns = [
+    url(r'^news/(?P<pk>\d+)$', NewsDetailView.as_view(), name='news_detail'),
     url(r'^news/$', NewsViewList.as_view(), name='news'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import News
 from categories.models import Category
@@ -12,4 +13,13 @@ class NewsViewList(ListView):
     def get_context_data(self, **kwargs):
         context = super(NewsViewList, self).get_context_data(**kwargs)
         context['catalogs'] = Category.objects.values('id', 'name')
+        return context
+
+
+class NewsDetailView(DetailView):
+    template_name = 'news_detail.html'
+    model = News
+
+    def get_context_data(self, **kwargs):
+        context = super(NewsDetailView, self).get_context_data(**kwargs)
         return context
